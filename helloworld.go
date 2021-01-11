31f6e19f4f10901f5adf91be6e8a271d23bdc567
Hannah Zhang <hannahjzhang@gmail.com>
2021-01-10 21:09:54 -0800

    local hack day

---
+++ helloworld.go
@@ -0,0 +1,11 @@
+// every go file must start with a package name statement
+package main
+
+// fmt package is imported to print text
+import "fmt"
+
+// a special function where the program execution starts
+func main() {
+  // used to write text to the standard output
+  fmt.Println("Hello, World")
+}
